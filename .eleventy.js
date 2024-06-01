7ce4c66a7f3d9bdb7ce78cb2f38e58804695d0c7
Rendra Sukana <rendra@cnaindo.com>
2024-06-01 09:13:25 +0700

    feat: configured translation plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const { EleventyI18nPlugin } = require("@11ty/eleventy");
+const i18n = require('eleventy-plugin-i18n');
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets", {overwrite: true, expand: true});
@@ -11,4 +12,16 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addFilter("locale_url", function(url, page) {
         return `/${ page.lang || "en" }${url}`
     });
+
+    eleventyConfig.addPlugin(i18n, {
+        translations: {
+          title: {
+            'en': 'Mulia Jasa Indokor - Car Rental Company',
+            'ko': '물리아 야사 인도코르(Mulia Jasa Indokor) - 자동차 렌트 회사'
+          }
+        },
+        fallbackLocales: {
+          'ko': 'en'
+        }
+      });
 };
