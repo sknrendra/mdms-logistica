40701e4b59e43cb5a4ff157f90f2d0e25f03bd66
Rendra Sukana <rendra@cnaindo.com>
2024-06-01 10:19:35 +0700

    feat: added locale switch on navbar

--- .eleventy.js
+++ .eleventy.js
@@ -13,12 +13,53 @@ module.exports = function(eleventyConfig) {
         return `/${ page.lang || "en" }${url}`
     });
 
+    eleventyConfig.addFilter("toKorean", function(url, page) {
+        return `${url.replace('/en/', '/ko/')}`
+    });
+
+    eleventyConfig.addFilter("toEnglish", function(url, page) {
+        return `${url.replace('/ko/', '/en/')}`
+    });
+
     eleventyConfig.addPlugin(i18n, {
         translations: {
           title: {
             'en': 'Mulia Jasa Indokor - Car Rental Company',
             'ko': '물리아 야사 인도코르(Mulia Jasa Indokor) - 자동차 렌트 회사'
+          },
+          'home': {
+            'en': 'Home',
+            'ko': '홈 페이지'
+          },
+          'how-it-works': {
+            'en': 'How It Works',
+            'ko': '이용 방법'
+          },
+          'services': {
+            'en': 'Services',
+            'ko': '서비스'
+          },
+          'individual-rental': {
+            'en': 'Individual Rental',
+            'ko': '기업 렌탈'
+          },
+          'corporate-rental': {
+            'en': 'Corporate Rental',
+            'ko': '개인 렌탈'
+          },
+          'about-us': {
+            'en': 'About Us',
+            'ko': '회사 소개'
+          },
+          'corporate-history': {
+            'en': 'Corporate History',
+            'ko': '기업 역사'
+          },
+          'contact-us': {
+            'en': 'Contact Us',
+            'ko': '연락처'
           }
+          
         },
         fallbackLocales: {
           'ko': 'en'
